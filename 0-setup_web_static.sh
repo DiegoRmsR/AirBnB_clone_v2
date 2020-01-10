f44ff4b236c2529d92af551f2e3b349d30ab5e62
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-01-10 01:13:32 +0000

    [Feature] sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,11 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo service nginx start
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
 echo "test Nginx" >> /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sudo sed -i "37i\\\n\tlocation hbnb_static/ {\n\t\t alias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "37i\\\n\tlocation hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
