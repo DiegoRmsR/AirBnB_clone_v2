3e30b5a9e4fc49f1889f924bf5ddbf8c4cb281cd
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-01-11 02:36:02 +0000

    [Feature] Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,13 +21,13 @@ def do_deploy(archive_path):
         web_name = filename[0]
         dire_file = "/data/web_static/releases/{}/".format(web_name)
         put(archive_path, '/tmp/')
-        run("mkdir -p {}{}/".format(dire_file))
-        run("tar -xzf /tmp/{} -C {}{}/".format(file_name, dire_file))
+        run("mkdir -p {}/".format(dire_file))
+        run("tar -xzf /tmp/{} -C {}/".format(file_name, dire_file))
         run("rm /tmp/{}".format(file_name))
-        run("mv {}{}/web_static/* {}{}/".format(dire_file, dire_file))
-        run("rm -rf {}{}web_static".format(dire, web_name))
+        run("mv {}/web_static/* {}/".format(dire_file, dire_file))
+        run("rm -rf {}web_static".format(dire_file))
         run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dire_file))
+        run("ln -s {}/ /data/web_static/current".format(dire_file))
         print("New version deployed!")
         return True
 
