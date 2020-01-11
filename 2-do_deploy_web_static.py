1bcc9e81d32e5614bb897a0a9ba327f230bfcb20
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-01-11 02:20:57 +0000

    [Feature] Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using the
+function do_deploy """
+
+from fabric.api import *
+import os
+
+
+env.hosts = ['34.73.121.160', '35.185.87.206']
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    """ Do deploy """
+    if not os.path.exists(archive_path):
+            return False
+    try:
+        file_name = os.path.basename(archive_path)
+        filename = file_name.split(".")
+        web_name = filename[0]
+        dire = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run("mkdir -p {}{}/".format(dire, web_name))
+        run("tar -xzf /tmp/{} -C {}{}/".format(file_name, dire, web_name))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}{}/web_static/* {}{}/".format(dire, web_name, dire, web_name))
+        run("rm -rf {}{}web_static".format(dire, web_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dire, web_name))
+        print("New version deployed!")
+        return True
+
+    except:
+        return False
