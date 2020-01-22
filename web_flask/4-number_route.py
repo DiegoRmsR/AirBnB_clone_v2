2831f8394a885405f15851fd52dd700adb50a5e4
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-01-22 02:42:51 -0500

    [Feature] script that starts a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """route / that display Hello HBNB"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """route /hbnb that display HBNB"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def d_text(text):
+    """route /c/<text> that display C + text"""
+    text = text.replace('_', ' ')
+    return ("C " + text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """route that display Python + text"""
+    text = text.replace('_', ' ')
+    return ("Python " + text)
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    """route that siplay n if is an integer"""
+    if n is type(int):
+        return (n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
