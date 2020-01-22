11419b9925260dd7fb6de165eb92027bbe7c769f
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-01-22 00:39:47 -0500

    [Feature] script that starts a Flask web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
-    """funtion whit route"""
+    """route / that display Hello HBNB"""
     return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """funtion whit route that display HBNB"""
+    """route /hbnb that display HBNB"""
     return ("HBNB")
 
 if __name__ == "__main__":
