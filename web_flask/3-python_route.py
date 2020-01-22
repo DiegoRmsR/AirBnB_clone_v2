af5b5515414a7f94bb6cb8d8238a1a6ee27e0656
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-01-22 02:11:31 -0500

    [Feature] script that starts a Flask web application

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """route / that display Hello HBNB"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """route /hbnb that display HBNB"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def d_text(text):
+    """route /c/<text> that display C + text"""
+    text = text.replace('_', ' ')
+    return ("C " + text)
+
+
+@app.route('/python/(<text>)', strict_slashes=False)
+def python_text(text='is cool'):
+    """route that display python + text"""
+    text = text.raplace('_', ' ')
+    return ("Python" + text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
